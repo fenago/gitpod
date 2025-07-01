6ea7f6aa1aa939ae41a6847d2b0df2da88c50e4d
Dr. Ernesto Lee <socrates73@gmail.com>
2025-07-01 08:37:51 -0400

    Add files via upload

---
+++ src/test/java/com/clouddev/api/taskmanager/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clouddev.api.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
